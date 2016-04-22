1fc83f036c028cb9b7f2f2a3fb0128ab166e2d07
swtnext <swtnext@gmail.com>
2016-04-22 14:43:51 +0200

    migrate to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20160420110319) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string   "name",                                null: false
     t.string   "last_name",                           null: false
     t.string   "email",                  default: "", null: false
