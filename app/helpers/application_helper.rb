5a93d320ebbc0311ed2143deefae34589776eb8f
swtnext <swtnext@gmail.com>
2016-04-22 15:24:17 +0200

    style fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,17 @@ module ApplicationHelper
 
   def bootstrap_class_for flash_type
     case flash_type
-      when :success
-        "alert-success"
-      when :error
-        "alert-error"
-      when :alert
-        "alert-block"
+      when 'success'
+        'alert-success'
+      when 'notice'
+        'alert-info'
+      when 'alert'
+        'alert-danger'
       when :notice
-        "alert-info"
+        'alert-info'
       else
         flash_type.to_s
     end
   end
-  
+
 end
