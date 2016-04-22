03cbc0df7da58e05c63534fd45a62a26c7760fc6
swtnext <swtnext@gmail.com>
2016-04-22 13:14:53 +0200

    some more tests refactor

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 describe 'Login', type: :feature do
   it 'successfully signs in' do
-    user = create(:user)
-    auth_helper = AuthHelper.new
-    auth_helper.login user
+    AuthHelper.new.login create(:user)
     expect(page).to have_content('Signed in successfully.')
   end
 end
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 describe 'Registration', type: :feature do
   it 'successfully signs up a user' do
-    user = build(:user)
-    auth_helper = AuthHelper.new
-    auth_helper.register user
+    AuthHelper.new.register build(:user)
     expect(page).to have_content('Welcome! You have signed up successfully')
   end
 end
